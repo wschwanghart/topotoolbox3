fa5cff6a91d5b8890e5e332f02603726f4885f21
William Kearney <will@wskearney.com>
2024-09-04 09:14:19 +0200

    Add GitHub Actions workflow
    Resolves #2
    
    .github/workflow/ci.yaml uses matlab-actions/setup-matlab@v2 to
    install the latest release of MATLAB on the runner and then runs the
    tasks in buildfile.m using matlab-actions/run-build@v2. Note that the
    tests require the Mapping Toolbox and Image Processing Toolbox which
    we install using the `products` property in the setup-matlab task. Any
    additional products that are needed by the tests in the future must
    be added here. The names of the products can be found at
    https://raw.githubusercontent.com/mathworks-ref-arch/matlab-dockerfile/main/mpm-input-files/R2024a/mpm_input_r2024a.txt
    
    buildfile.m now uses buildplan(localfunctions) to create the "test"
    task rather than using the default TestTask. This allows us to include
    `toolbox/` on the path and ensure that the tests can see the toolbox
    code.

--- buildfile.m
+++ buildfile.m
@@ -1,12 +1,20 @@
 function plan = buildfile
-    plan = buildplan;
+    plan = buildplan(localfunctions);
     
     plan("check") = matlab.buildtool.tasks.CodeIssuesTask("toolbox");
-    plan("test") = matlab.buildtool.tasks.TestTask("tests");
     plan("package") = matlab.buildtool.Task( ...
         Description = "Package toolbox", ...
         Dependencies = ["check" "test"], ...
         Actions = @packageToolbox);
 
     plan.DefaultTasks = ["check" "test" "package"];
+end
+
+function testTask(~)
+    oldpath = addpath(genpath("toolbox"));
+    finalize = onCleanup(@()(path(oldpath)));
+
+    results = runtests("IncludeSubfolders",true);
+    disp(results);
+    assertSuccess(results);
 end
