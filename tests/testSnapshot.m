6b047c3df60eafdd5a2d9049fc4f241b42cc47cc
William Kearney <william.kearney@uni-potsdam.de>
2024-10-01 10:03:59 +0200

    Update documentation for the snapshot tests
    This also changes the name of the submodule, which should not be a problem.
    
    Signed-off-by: William Kearney <william.kearney@uni-potsdam.de>

--- tests/testSnapshot.m
+++ tests/testSnapshot.m
@@ -2,30 +2,63 @@ classdef testSnapshot < matlab.unittest.TestCase
     % testSnapshot Snapshot testing using topotoolbox3 as a reference
     % The data for the snapshot tests are stored in the
     % TopoToolbox/test_data repository. This repository is added as a
-    % submodule to topotoolbox3 at the path tests/data.
+    % submodule to topotoolbox3 at the path tests/snapshots.
+    %
+    % To obtain the snapshot test data, you must initialize the
+    % test/snapshots submodule. If you are cloning the repository for the
+    % first time, you can use `git clone --recurse-submodules
+    % https://github.com/TopoToolbox/topotoolbox3` to initialize the
+    % submodule. If you have already cloned the project you should run `git
+    % submodule update --init`. If either of those commands complete
+    % successfully, you should have some GeoTIFFs in your tests/snapshots
+    % directory.
+    %
+    % If you need to pull in new changes from the TopoToolbox/topotoolbox3
+    % repository that include changes to the snapshots submodule, you
+    % should run `git submodule update` after running `git pull` or run
+    % `git pull --recurse-submodules` to always pick up the latest
+    % submodule changes.
+    % 
+    % See the Pro Git page on submodules for more guidance:
+    % https://git-scm.com/book/en/v2/Git-Tools-Submodules
     %
     % The basic strategy for these tests:
     %
     % 1. The snapshot tests are run just like a normal test for
     %    topotoolbox3.
-    % 2. If there is no data for a given test in the tests/data directory,
-    %    the test will create it.
-    % 3. If the appropriate data does exist in the tests/data directory,
-    %    they will be compared against the results computed here. The test
-    %    will fail if the saved version and the computed version do not
-    %    match according to the test. Be careful when adding new tests
-    %    requiring floating point comparison.
+    % 2. If there is no data for a given test in the tests/snapshots
+    %    directory, the test will create it.    
+    % 3. If the appropriate data does exist in the tests/snapshots
+    %    directory, they will be compared against the results computed
+    %    here. The test will fail if the saved version and the computed
+    %    version do not match according to the test. 
     % 4. Snapshotting of results must currently be done manually. If you
     %    want to save the results of a test run:
-    %    - Commit the new data to the /submodule/, not to topoboolbox3
+    %    - Commit the new data to the /submodule/, not to topoboolbox3. If
+    %      you make the commit from the tests/snapshots directory, you
+    %      should be committing to the correct repository.
     %    - Push the commit to your fork of TopoToolbox/test_data and make a
-    %    pull request.
+    %      pull request.
     %    - Once the pull request is merged, pull the new changes into the
-    %    submodule and commit the changed submodule to the
-    %    topotoolbox3 repository.
-    % 5. If a change to the results is known and expected, delete the
-    %    appropriate snapshot from the tests/data repository and create the
-    %    new one following the procedure outlined above.
+    %      submodule by going into the submodule directory and running the
+    %      appropriate `git pull` command. 
+    %    - If you now move to the topotoolbox3 directory and run `git
+    %      status` you should see changes to tests/snapshots, but not
+    %      to any of the files within that directory. `git add
+    %      tests/snapshots` and `git commit` to record the new test_data
+    %      commit for the submodule.
+    %    - Now push the topotoolbox3 commit to your fork and make a pull
+    %      request. Once it is merged, you should be able to run `git pull
+    %      --recurse-submodules` to pull the new changes including the new
+    %      snapshot data into your main branch.
+    % 5. If a change to the results is expected, delete the appropriate
+    %    snapshot from the tests/snapshots directory before running the
+    %    test and create a new one following the procedure outlined above.
+    %
+    % Be careful when adding new tests requiring floating point comparison.
+    % These tests may work on your own machine, but might vary across
+    % different operating systems and hardware architectures. Use
+    % approximate comparisons where appropriate.
 
     properties
         dem
