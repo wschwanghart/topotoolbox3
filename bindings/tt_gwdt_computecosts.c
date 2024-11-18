2934720a9131fc498766dce3d28b0ad932ba2c7b
William Kearney <william.kearney@uni-potsdam.de>
2024-11-18 11:13:30 +0100

    Add MEX skeleton for gwdt_computecosts
    This is the starting point for implementing a MEX binding to
    libtopotoolbox. All MEX functions have the same signature
    
    ```
    void mexFunction(int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[]);
    ```
    
    and we must use the MEX headers "matrix.h" and "mex.h" as well as the
    libtopotoolbox header "topotoolbox.h".
    
    I have also added comments to guide the implementation of the binding
    within the MEX function.

---
+++ bindings/tt_gwdt_computecosts.c
@@ -0,0 +1,21 @@
+/*
+
+tt_gwdt_computecosts.c
+
+*/
+
+#include "matrix.h"
+#include "mex.h"
+#include "topotoolbox.h"
+
+void mexFunction(int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[]) {
+  // Validate input and output arguments
+
+  // Extract input and output array data and dimensions
+
+  // Allocate necessary intermediate arrays
+
+  // Call libtopotoolbox function
+
+  // Destroy intermediate arrays
+}
