dd054cac31fd675e330f927489385b6bc78146f4
William Kearney <william.kearney@uni-potsdam.de>
2024-11-18 14:34:59 +0100

    Extract input and create output arrays
    This uses the mxGet* functions to obtain pointers to the various input
    arrays and the mxCreateNumericMatrix function to allocate the single
    output array. The output array is managed by MATLAB and does not need
    to be freed.
    
    This will still not compile because the intermediate arrays have not
    been created.

--- bindings/tt_gwdt_computecosts.c
+++ bindings/tt_gwdt_computecosts.c
@@ -10,7 +10,11 @@ COSTS = tt_gwdt_computecosts(DEMF, DEM, FLATS)
 #include "mex.h"
 #include "topotoolbox.h"
 
-void mexFunction(int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[]) {  
+#include <limits.h>
+#include <stddef.h>
+#include <stdint.h>
+
+void mexFunction(int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[]) {
   // Validate input and output arguments
   if (nrhs != 3) {
     mexErrMsgIdAndTxt("tt3:tt_gwdt_computecosts:nrhs", "Three inputs required");
@@ -18,8 +22,48 @@ void mexFunction(int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[]) {
   if (nlhs != 1) {
     mexErrMsgIdAndTxt("tt3:tt_gwdt_computecosts:nlhs", "One output required");
   }
-  
   // Extract input and output array data and dimensions
+  const mxArray *demArray = prhs[0]; // DEM
+  float *original_dem = mxGetSingles(demArray);
+
+  const mxArray *demfArray = prhs[1]; // DEMF
+  float *filled_dem = mxGetSingles(demArray);
+
+  const mxArray *flatsArray = prhs[2]; // FLATS
+  int32_t *flats = mxGetInt32s(flatsArray);
+
+  // Create the dimensions.
+  //
+  // This is always an array of two ptrdiff_t.
+  //
+  // A ptrdiff_t is equivalent to mwSignedIndex. MATLAB's mxGetM and
+  // mxGetN return mwSize, which is a size_t, i.e. an unsigned
+  // integer. Converting from size_t to a ptrdiff_t is unsafe, because
+  // the latter is not guaranteed to be large enough to hold a
+  // size_t. In general we don't check this because it is not feasible
+  // to have an array of single-precision floating point numbers with
+  // a size that fits in a size_t but not a ptrdiff_t. Here, I do show
+  // how to check it for completeness.
+
+  ptrdiff_t dims[2];
+  mwSize m = mxGetM(demArray);
+  mwSize n = mxGetN(demArray);
+
+  if (m > PTRDIFF_MAX) {
+    mexErrMsgIdAndTxt("tt3:tt_gwdt_computecosts:mxGetM",
+                      "Dimension does not fit in a ptrdiff_t");
+  }
+  if (n > PTRDIFF_MAX) {
+    mexErrMsgIdAndTxt("tt3:tt_gwdt_computecosts:mxGetN",
+                      "Dimension does not fit in a ptrdiff_t");
+  }
+
+  dims[0] = m;
+  dims[1] = n;
+
+  // Create output array
+  plhs[0] = mxCreateNumericMatrix(dims[0], dims[1], mxSINGLE_CLASS, mxREAL);
+  float *costs = mxGetSingles(plhs[0]);
 
   // Allocate necessary intermediate arrays
 
