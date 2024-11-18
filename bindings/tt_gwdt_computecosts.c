6a1358e94c6614a0efc70fb10da007e770f222c9
William Kearney <william.kearney@uni-potsdam.de>
2024-11-18 14:07:20 +0100

    Validate the function interface
    I have yet to develop a complete set of interface checks for the
    libtopotoolbox bindings, and I usually just check that there are the
    right number of input and output arrays. One could do more such as
    checking the types or the sizes of the arrays. Since the bindings are
    not intended to be used directly, we can probably get away with less
    validation in the MEX function. Developing better interface validation
    is worth thinking about as we go forward: perhaps the validation code
    is a good way to document the interface more thoroughly.

--- bindings/tt_gwdt_computecosts.c
+++ bindings/tt_gwdt_computecosts.c
@@ -2,15 +2,23 @@
 
 tt_gwdt_computecosts.c
 
+COSTS = tt_gwdt_computecosts(DEMF, DEM, FLATS)
+
 */
 
 #include "matrix.h"
 #include "mex.h"
 #include "topotoolbox.h"
 
-void mexFunction(int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[]) {
+void mexFunction(int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[]) {  
   // Validate input and output arguments
-
+  if (nrhs != 3) {
+    mexErrMsgIdAndTxt("tt3:tt_gwdt_computecosts:nrhs", "Three inputs required");
+  }
+  if (nlhs != 1) {
+    mexErrMsgIdAndTxt("tt3:tt_gwdt_computecosts:nlhs", "One output required");
+  }
+  
   // Extract input and output array data and dimensions
 
   // Allocate necessary intermediate arrays
