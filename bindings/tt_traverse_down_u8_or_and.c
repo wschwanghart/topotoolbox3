a5740034faa4716f70cab8fb2008e33f726aa233
William Kearney <william.kearney@uni-potsdam.de>
2026-06-25 13:04:02 +0200

    Use libtopotoolbox in dependencemap (#183)
    See #171
    
    This PR adds bindings for traverse_up_u8_or_and and
    traverse_down_u8_or_and from libtopotoolbox, which are used to compute
    dependencemap and influencemap, respectively.
    
    Two tests are added to testFlowAlgebra.m:
    
    - Check to ensure that the three input argument styles result in the
      correct answer (with native MATLAB and libtopotoolbox)
    
    - Check that libtopotoolbox produces the same answer as native MATLAB
    
    Signed-off-by: William Kearney <william.kearney@uni-potsdam.de>

---
+++ bindings/tt_traverse_down_u8_or_and.c
@@ -0,0 +1,63 @@
+/*
+
+  void traverse_down_u8_or_and(float *output, float *input,
+                                 ptrdiff_t *source,
+                                 ptrdiff_t *target,
+                                 ptrdiff_t edge_count)  
+ */
+
+#include "matrix.h"
+#include "mex.h"
+#include "topotoolbox.h"
+
+#include <limits.h>
+#include <stddef.h>
+#include <stdint.h>
+
+void mexFunction(int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[]) {
+  // [output] = tt_traverse_down_u8_or_and(output, input, source, target)
+
+  if (nrhs != 4) {
+    mexErrMsgIdAndTxt("tt3:tt_traverse_down_u8_or_and",
+                      "Four inputs required");
+  }
+
+  if (!mxIsClass(prhs[0], "uint8")) {
+    mexErrMsgIdAndTxt("tt3:tt_traverse_down_u8_or_and",
+                      "output argument must be of class uint8");
+  }
+
+  if (!mxIsClass(prhs[1], "uint8")) {
+    mexErrMsgIdAndTxt("tt3:tt_traverse_down_u8_or_and",
+                      "input argument must be of class uint8");
+  }
+
+  if (!mxIsClass(prhs[2], "int64")) {
+    mexErrMsgIdAndTxt("tt3:tt_traverse_down_u8_or_and",
+                      "source argument must be of class int64");
+  }
+
+  if (!mxIsClass(prhs[3], "int64")) {
+    mexErrMsgIdAndTxt("tt3:tt_traverse_down_u8_or_and",
+                      "target argument must be of class int64");
+  }
+
+  if (nlhs != 1) {
+    mexErrMsgIdAndTxt("tt3:tt_traverse_down_u8_or_and", "One output required");
+  }
+
+  mwSize edge_count = mxGetM(prhs[1]);
+  mwSize m = mxGetM(prhs[0]);
+  mwSize n = mxGetN(prhs[1]);
+
+  // MATLAB does not want us to modify input data, so we must copy the
+  // `output` parameter.
+  plhs[0] = mxDuplicateArray(prhs[0]);
+  uint8_t *output = mxGetUint8s(plhs[0]);
+  uint8_t *input = mxGetUint8s(prhs[1]);
+
+  ptrdiff_t *source = (ptrdiff_t *)mxGetInt64s(prhs[2]);
+  ptrdiff_t *target = (ptrdiff_t *)mxGetInt64s(prhs[3]);
+
+  traverse_down_u8_or_and(output, input, source, target, edge_count);
+}
